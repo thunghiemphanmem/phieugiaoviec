3b3c4a50496813f4c7077979e703815ce4117a7e
thunghiemphanmem <hosonhatruong.c1ht@elc.vn>
2026-06-04 21:52:10 +0700

    Add files via upload

---
+++ ketnoi.js
@@ -0,0 +1,13 @@
+/* ========================================================================== */
+var ddungApi = "https://script.google.com/macros/s/AKfycbz6t2J2H3RNA3CY9KXGv9M0_JZ2djNYeHf-fsMYfX97HGDvYDdWu0Jf7eemuiI9KmJS/exec"; 
+
+// --- CÁC BIẾN CẤU HÌNH ĐỊNH DANH HỆ THỐNG ---
+var PGV_LINK_LOGO = "https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png"; 
+var PGV_TEN_TRUONG = "Trường TH&THCS Hợp Thành";
+var PGV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
+var PGV_TEN_PM = "Quản lý Nhiệm vụ";
+var PGV_TEN_PM_UP = "QUẢN LÝ CÔNG VIỆC";
+var PGV_PHIEN_BAN = "V2.3.2026";
+var PGV_TAC_GIA = "HOÀNG NGỌC LÂM";
+var PGV_GOOGLE_CLIENT_ID = "808915800369-47d1eeka1a6qecttke9mkdndfco7dlls.apps.googleusercontent.com";
+var PGV_MO_TA = "Hệ thống quản lý hiệu quả công việc nội bộ - " + PGV_TEN_TRUONG;
